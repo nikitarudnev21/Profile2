f55fffda2c5b09bee774df8b46efbc5f5e7372aa
Opilane TTHK <opilane@tthk.ee>
2019-11-18 15:42:43 +0200

    Profile Pictures

--- github.js
+++ github.js
@@ -11,10 +11,18 @@ class GitHub{
 
     async getUserData(){
         //`` - backticks
-        let urlProfile= `https://api.github.com/${this.user}?client_id=${this.client_id}&client_secret=${this.client_secret}`;
-        let urlRepos= `https://api.github.com/${this.user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`;
+        let urlProfile= `https://api.github.com/users/${this.user}?client_id=${this.client_id}&client_secret=${this.client_secret}`;
+        let urlRepos= `https://api.github.com/users/${this.user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`;
         
         const profileResponse = await fetch(urlProfile);
         const reposResponse = await fetch(urlRepos)
+
+        const profile = await profileResponse.json();
+        const repos =  await reposResponse.json();
+
+        return{
+            profile,
+            repos
+        }
     }
 }
